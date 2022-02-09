b852a3acc88c134d99c28b66c94c73cfd226a4db
romad <romadmitr12@gmail.com>
2022-02-09 23:11:47 +0700

    Сделана основная работа. Фильмы обрабатывают get post delete put. Сортировка по id режиссеров и жанров.

--- app.py
+++ app.py
@@ -63,26 +63,68 @@ class GenreSchema(Schema):
 @movies_ns.route('/')
 class MoviesView(Resource):
     def get(self):
-
-        all_movies = Movie.query.all()
+        genre_id = request.args.get('genre_id')
+        director_id = request.args.get('director_id')
+        print(genre_id)
+        print(director_id)
+        if genre_id or director_id:
+            all_movies_filter = \
+                db.session.query(Movie).filter(Movie.genre_id == genre_id, Movie.director_id == director_id)
+            return MovieSchema(many=True).dump(all_movies_filter), 200
+        if genre_id:
+            all_movies_filter = \
+                db.session.query(Movie).filter(Movie.genre_id == genre_id)
+            return MovieSchema(many=True).dump(all_movies_filter), 200
+        if director_id:
+            all_movies_filter = \
+                db.session.query(Movie).filter(Movie.director_id == director_id)
+            return MovieSchema(many=True).dump(all_movies_filter), 200
+        all_movies = db.session.query(Movie).all()
         return MovieSchema(many=True).dump(all_movies), 200
 
     def post(self):
         req_json = request.json
         new_movie = Movie(**req_json)
+
         with db.session.begin():
             db.session.add(new_movie)
-        return "", 200
+
+        return "", 201
 
 
 @movies_ns.route('/<int:uid>')
 class MovieView(Resource):
     def get(self, uid: int):
         try:
-            movie = Movie.query.get(uid)
+            movie = db.session.query(Movie).filter(Movie.id == uid).one()
             return MovieSchema().dump(movie), 200
         except Exception as e:
-            return "", 404
+            return str(e), 404
+
+    def put(self, uid: int):
+        movie = db.session.query(Movie).get(uid)
+        req_json = request.json
+        movie.id = req_json.get("id")
+        movie.title = req_json.get("title")
+        movie.description = req_json.get("description")
+        movie.trailer = req_json.get("trailer")
+        movie.year = req_json.get("year")
+        movie.rating = req_json.get("rating")
+        movie.genre_id = req_json.get("genre_id")
+        movie.director_id = req_json.get("director_id")
+
+        with db.session.begin():
+            db.session.add(movie)
+
+        return "", 204
+
+    def delete(self, uid: int):
+        movie = db.session.query(Movie).get(uid)
+
+        with db.session.begin():
+            db.session.delete(movie)
+
+        return "", 204
 
 
 if __name__ == '__main__':
